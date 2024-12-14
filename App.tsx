f1dd0d26eae927e81c787495204be11a6817d920
sudhanshu2070 <sssallen2016@gmail.com>
2024-12-14 18:27:52 +0530

    yoshe App V.1.0.1

--- App.tsx
+++ App.tsx
@@ -77,8 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Welcome to the <Text style={styles.highlight}>New World</Text>
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
